97f8e1ca27ae088b0329096de7b06d68db08421a
Yllcaka <yllcaka1516457@gmail.com>
2020-03-20 23:57:09 +0100

    Made OnlineTextToSpeech.py better

--- Map.py
+++ Map.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python3
 import requests,sys
 address = "".join(sys.argv[1])
-res = requests.get(address)
+res = requests.get(f"https://google.com/maps/{address}")
 print(res.status_code)
 # print(len(res.text))
-print(res.text[:int(sys.argv[2])])
+print(res.text[:int(sys.argv[1])])
 with open("Site.txt","wb") as site:
     for chunk in res.iter_content(100000):
         site.write(chunk)
--- OnlineTextToSpeech.py
+++ OnlineTextToSpeech.py
@@ -1,11 +1,15 @@
 #! /usr/bin/python3
 from selenium import webdriver
 import sys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 browser = webdriver.Firefox()
 browser.get("https://www.google.com/search?source=hp&ei=EPZ0XsmeCquVlwSk54qgAw&q=translate&oq=translate&gs_l=psy-ab.3..0i131j0l9.322.1245..1453...0.0..0.210.1229.0j7j1......0....1..gws-wiz.S9lZXZqULVM&ved=0ahUKEwjJ5oD9wqnoAhWryoUKHaSzAjQQ4dUDCAU&uact=5")
 browser.find_element_by_id("tw-source-text-ta").clear()
 try:
-    browser.find_element_by_id("tw-source-text-ta").send_keys(sys.argv[1].capitalize())
+    browser.find_element_by_id("tw-source-text-ta").send_keys(" ".join(sys.argv[1:]).capitalize())
 except IndexError:
     browser.find_element_by_id("tw-source-text-ta").send_keys("When launching the program enter something for it to work")
+WebDriverWait(browser, 1000000).until(EC.element_to_be_clickable((By.ID, 'tw-source-text-ta'))).click()
 browser.find_element_by_id("tw-src-spkr-button").click()
