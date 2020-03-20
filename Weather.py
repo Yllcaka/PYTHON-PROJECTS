068978eea85b951550723da52d4f188c1c3bdc66
Yllcaka <yllcaka1516457@gmail.com>
2020-03-20 16:10:26 +0100

    Added Weather.py

---
+++ Weather.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+import bs4, requests,re
+def temperature(temp):
+    regex = re.compile("\d+")
+    final = regex.search(temp[0].text).group()
+    return int(final)
+url = "https://www.bbc.com/weather/786714"
+site = requests.get(url)
+site.raise_for_status()
+soup = bs4.BeautifulSoup(site.text,"html.parser")
+weather = soup.select("#daylink-0 > div.wr-day__body > div.wr-day__weather-type-description-container > div")
+low = soup.select("#daylink-0 > div.wr-day__body > div.wr-day__details-container > div > div.wr-day__temperature > div > div.wr-day-temperature__high > span.wr-day-temperature__high-value > span > span.wr-value--temperature--c")
+high = soup.select("#daylink-0 > div.wr-day__body > div.wr-day__details-container > div > div.wr-day__temperature > div > div.wr-day-temperature__low > span.wr-day-temperature__low-value > span > span.wr-value--temperature--c")
+print(f"WEATHER REPORT:\n{weather[0].text}\nTemperature from {low[0].text} to {high[0].text}")
+print(f"With an average Temperature of: {(temperature(low)+temperature(high)/2)}")
