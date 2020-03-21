daab1e60f990d6e0fc08955559f30da63d0b901d
Yllcaka <yllcaka1516457@gmail.com>
2020-03-21 20:24:38 +0100

    Changed Weather.py website to Weather2umbrella

--- Weather.py
+++ Weather.py
@@ -1,18 +1,20 @@
 #! /usr/bin/python3
 import bs4, requests,re
+# spaces = re.compile("\s{2,}")
 def temperature(temp):
     regex = re.compile("\d+")
     final = regex.search(temp[0].text).group()
     return int(final)
-url = "https://www.bbc.com/weather/786714"
+url = "https://www.weather2umbrella.com/weather-forecast-pristina-serbia-en/7-days"
 site = requests.get(url)
 try:
     site.raise_for_status()
     soup = bs4.BeautifulSoup(site.text,"html.parser")
-    weather = soup.select("#daylink-0 > div.wr-day__body > div.wr-day__weather-type-description-container > div")
-    low = soup.select("#daylink-0 > div.wr-day__body > div.wr-day__details-container > div > div.wr-day__temperature > div > div.wr-day-temperature__high > span.wr-day-temperature__high-value > span > span.wr-value--temperature--c")
-    high = soup.select("#daylink-0 > div.wr-day__body > div.wr-day__details-container > div > div.wr-day__temperature > div > div.wr-day-temperature__low > span.wr-day-temperature__low-value > span > span.wr-value--temperature--c")
-    print(f"WEATHER REPORT:\n{weather[0].text}\nTemperature from {low[0].text} to {high[0].text}")
-    print(f"With an average Temperature of: {(temperature(low)+temperature(high)/2)}")
-except:
+    weather = soup.select("#seven_days > div:nth-of-type(2)) > div > div > div > a.day_wrap.today.selected_day > div.weather_per_day_wrap > div.day_description > p")
+    low = soup.select("#seven_days > div:nth-of-type(2) > div > div > div > a.day_wrap.today.selected_day > div.weather_per_day_wrap > div.day_temperatures > div.min_day_temp_section > p")
+    high = soup.select("#seven_days > div:nth-of-type(2) > div > div > div > a.day_wrap.today.selected_day > div.weather_per_day_wrap > div.day_temperatures > div.max_day_temp_section > p")
+    print(f"WEATHER REPORT:{' '.join((weather[0].text.strip()).split())}\nTemperature from {(low[0].text).strip()} to {(high[0].text).strip()}")
+    print(f"With an average Temperature of: {(temperature(low)+temperature(high))/2}")
+except Exception as e:
     print("The site isn't corrently working")
+    print("Because:",e)
