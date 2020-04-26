0d973ce9762e9168ab23bfad718ebffe43f4a286
Ylli Caka <yllcaka1516457@gmail.com>
2020-04-26 21:08:31 +0200

    Added GameOfLife.py

---
+++ GameOfLife.py
@@ -0,0 +1,71 @@
+import pygame, copy
+import random as r
+from time import sleep
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+def make2dArray(col,row):
+    return [[None for i in range(row)] for i in range(col)]
+def firstArray(col,row):
+    return [[r.randint(0,1) for i in range(row)] for i in range(col)]
+        
+def fate(spot,x,y,cols,rows):
+    """
+    This 
+    """
+    sum = 0
+    for i in range(-1,2):
+        for j in range(-1,2):
+            try: 
+                col = x+i
+                row = y+j
+                sum+= spot[col][row]
+            except:
+                continue
+    sum -= spot[x][y]
+    if spot[x][y] == 0 and sum == 3:
+        return 1
+    elif spot[x][y] == 1 and (sum <2 or sum >3):
+        return 0
+    else:
+        return spot[x][y]
+
+pygame.init()
+cols = 0
+rows = 0
+resolution = 20
+m = 1
+
+width = 1400
+height = 800
+cols = width//resolution
+rows = height//resolution
+life = firstArray(cols,rows)
+windowSize = (width-m,height-m)
+screen = pygame.display.set_mode(windowSize)
+
+pygame.display.set_caption("Game of Life")
+
+running = True
+current = make2dArray(cols,rows)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(white)
+    for i in range(cols):
+        for j in range(rows):
+            currentBeing = life[i][j]
+            color = white
+            if currentBeing == 1:
+                color = black
+            x = i*resolution
+            y = j*resolution
+            pygame.draw.rect(screen,color,[x,y,resolution-m,resolution-m])
+    for i in range(cols):
+        for j in range(rows):
+            current[i][j] = fate(life,i,j,cols,rows)
+    sleep(0.05)
+    life = copy.deepcopy(current)
+   
+    pygame.display.flip()
