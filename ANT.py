f27c29d2a0c46d37866f919ce53b3b2d4e139864
Ylli Caka <yllcaka1516457@gmail.com>
2020-05-04 01:20:17 +0200

    Added a pygame implimentation of "Langton's\ Ant"

---
+++ ANT.py
@@ -0,0 +1,90 @@
+import pygame, copy
+from time import sleep
+
+class Ant:
+    def __init__(self,arr:list,direction:str,x,y):
+        self.arr = arr
+        self.direction = direction.upper()
+        self.x = x
+        self.y = y
+        self.position = self.arr[self.x][self.y]
+
+    def antPosition(self,dir:str):
+        x = self.x
+        y = self.y
+
+        Directions = {"UP":(x,y-1),"RIGHT":(x+1,y),"DOWN":(x,y+1),"LEFT":(x-1,y)}
+        return Directions[dir]
+    
+    def changeAntDirection(self,color:int,dir:str):
+        DirectionClockWise = {"UPbegin":"RIGHT","RIGHTbegin":"DOWN","DOWNbegin":"LEFT","LEFTbegin":"UP"}
+        DirectionCounterClockWise = {"UPbegin":"LEFT","LEFTbegin":"DOWN","DOWNbegin":"RIGHT","RIGHTbegin":"UP"}
+        return DirectionCounterClockWise[dir] if color ==1 else DirectionClockWise[dir] 
+
+    def move(self):
+        if self.position == 1:
+            self.arr[self.x][self.y] = 0
+            self.direction = self.changeAntDirection(1,self.direction+"begin")
+            self.x,self.y = self.antPosition(self.direction)
+            self.position = self.arr[self.x][self.y]
+            
+            self.arr[self.x][self.y] =1
+        elif self.position == 0:
+            self.arr[self.x][self.y] = 1         
+            self.direction = self.changeAntDirection(0,self.direction+"begin")
+            self.x,self.y = self.antPosition(self.direction)
+            self.position = self.arr[self.x][self.y]
+
+            
+
+
+
+
+
+def make2dArray(col,row):
+    return [[0 for i in range(col)] for i in range(row)]
+
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+
+pygame.init()
+cols = 0
+rows = 0
+resolution = 10
+m = 1
+
+width = 1400
+height = 1000
+cols = height//resolution
+rows = width//resolution
+
+windowSize = (width-m,height-m)
+screen = pygame.display.set_mode(windowSize)
+
+running = True
+current = make2dArray(cols,rows)
+ANT = Ant(current,"UP",cols-15,rows//3)
+
+current = ANT.arr
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(black)
+    
+    for i in range(rows):
+        for j in range(cols):
+            currentBeing = current[i][j]
+            color = white
+            if currentBeing == 1:
+                color = black
+            x = i*resolution
+            y = j*resolution
+            pygame.draw.rect(screen,color,[x,y,resolution-m,resolution-m])
+    ANT.move()
+    current = ANT.arr
+    # sleep(1)
+    pygame.display.flip()
